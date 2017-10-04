01f13ff6f30f03933f1b883dca01a1d48191ca4c
DrewDahlman <info@drewdahlman.com>
2017-10-04 13:29:32 -0600

    updating watcher and header

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -21,5 +21,5 @@ gulp.task("watch", function(){
   gulp.watch( config.watchPath + "/scripts/**/**/*.js", gulp.parallel("scripts") );
   gulp.watch( config.watchPath + "/templates/**/**/*.html", gulp.parallel("scripts") );
   gulp.watch( config.watchPath + "/styles/**/**/*.sass", gulp.parallel("styles") );
-  gulp.watch( config.watchPath + "/markup/**/**/*.html", gulp.parallel("markup") );
+  gulp.watch( config.watchPath + "/assets/**/**/*", gulp.parallel("copy") );
 });
