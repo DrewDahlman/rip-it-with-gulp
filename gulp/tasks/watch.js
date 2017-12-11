49e67e47b3198619c5d585b5f91e8d17b44cc195
Drew Dahlman <info@drewdahlman.com>
2017-12-11 15:05:44 -0700

    adding markup to watchers

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -21,5 +21,6 @@ gulp.task("watch", function(){
   gulp.watch( config.watchPath + "/scripts/**/**/*.js", gulp.parallel("scripts") );
   gulp.watch( config.watchPath + "/templates/**/**/*.html", gulp.parallel("scripts") );
   gulp.watch( config.watchPath + "/styles/**/**/*.sass", gulp.parallel("styles") );
+  gulp.watch( config.watchPath + "/markup/**/**/*", gulp.parallel("markup") );
   gulp.watch( config.watchPath + "/assets/**/**/*", gulp.parallel("copy") );
 });
